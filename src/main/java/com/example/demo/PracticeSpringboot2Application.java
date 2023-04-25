3037b81966896d742d40e36943b28f5b7ec7a345
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-26 06:46:50 +0900

    "first_commit"

---
+++ src/main/java/com/example/demo/PracticeSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/PracticeSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
